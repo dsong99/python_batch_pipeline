a7f2f0966e0e4d59889552175ceb1148b08ec554
David Song <49618112+dsong99@users.noreply.github.com>
2025-06-19 15:41:11 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-import logging
-logging.basicConfig(
-    filename='batch.log',
-    level=logging.DEBUG,
-    format='%(asctime)s - %(process)d - %(thread)d - %(levelname)s - %(message)s'
-)
